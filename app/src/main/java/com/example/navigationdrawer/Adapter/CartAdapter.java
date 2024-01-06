522c1945f1c43d0c72757a40423e8092b3aa8c3a
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2024-01-06 06:05:43 +0300

    complete Cart page integration and start in favorite page

--- app/src/main/java/com/example/navigationdrawer/Adapter/CartAdapter.java
+++ app/src/main/java/com/example/navigationdrawer/Adapter/CartAdapter.java
@@ -1,38 +1,73 @@
 package com.example.navigationdrawer.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners;
 import com.example.navigationdrawer.Domain.CartProduct;
+import com.example.navigationdrawer.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.Viewholder> {
 
-    private ArrayList<CartProduct> items;
+    private List<CartProduct> items;
+    private Context context;
+
+    public CartAdapter(List<CartProduct> items) {
+        this.items = items;
+    }
 
     @NonNull
     @Override
     public CartAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View inflate= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_cart,parent, false);
+        context = parent.getContext();
+        return new Viewholder(inflate);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull CartAdapter.Viewholder holder, int position) {
-
+        holder.qtyTxt.setText(""+items.get(position).getQty());
+        holder.titleTxt.setText(""+items.get(position).getName());
+        holder.feeEachitem.setText("$"+items.get(position).getNewPrice());
+        holder.totalEachItem.setText(""+items.get(position).getTotal());
+        if (!items.get(position).getImage().isEmpty())
+            Glide.with(holder.itemView.getContext()).load(items.get(position).getImage()).into(holder.pic);
+        else {
+            Glide.with(holder.itemView.getContext())
+                    .load(R.drawable.pic1)
+                    .transform(new GranularRoundedCorners(30,30,0,0))
+                    .into(holder.pic);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     public class Viewholder extends  RecyclerView.ViewHolder {
+        ImageView pic;
+        TextView feeEachitem,titleTxt,qtyTxt,totalEachItem;
+
         public Viewholder(@NonNull View itemView) {
             super(itemView);
+            pic = (ImageView) itemView.findViewById(R.id.pic);
+            feeEachitem = (TextView) itemView.findViewById(R.id.feeEachitem);
+            titleTxt = (TextView) itemView.findViewById(R.id.titleTxt);
+            qtyTxt = (TextView) itemView.findViewById(R.id.qtyTxt);
+            totalEachItem = (TextView) itemView.findViewById(R.id.totalEachItem);
         }
     }
 }
--- app/src/main/java/com/example/navigationdrawer/Fragment/CartFragment.java
+++ app/src/main/java/com/example/navigationdrawer/Fragment/CartFragment.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.navigationdrawer.Adapter.CartAdapter;
 import com.example.navigationdrawer.Domain.CartProduct;
+import com.example.navigationdrawer.Domain.ErrorResponse;
 import com.example.navigationdrawer.Helper.RetrofitClient;
 import com.example.navigationdrawer.R;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,7 +26,7 @@ import retrofit2.Response;
 public class CartFragment extends Fragment {
 
     private RecyclerView listviewCart;
-    private RecyclerView.Adapter AdapterlistviewCart;
+    private RecyclerView.Adapter adapterlistviewCart;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -39,6 +44,16 @@ public class CartFragment extends Fragment {
             @Override
             public void onResponse(Call<List<CartProduct>> call, Response<List<CartProduct>> response) {
                 List<CartProduct> products = response.body();
+                if (response.isSuccessful()){
+                    listviewCart.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+                    adapterlistviewCart = new CartAdapter(products);
+                    listviewCart.setAdapter(adapterlistviewCart);
+
+                }else {
+                    ResponseBody errorResponse = response.errorBody();
+
+                    Toast.makeText(getActivity().getApplicationContext(), "Error on Cart Request"+errorResponse.toString(), Toast.LENGTH_SHORT).show();
+                }
 
             }
 
@@ -50,7 +65,6 @@ public class CartFragment extends Fragment {
     }
 
     private void initView(View view) {
-
-
+        listviewCart = view.findViewById(R.id.listviewCart);
     }
 }
